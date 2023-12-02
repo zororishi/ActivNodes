268062343313fd6f415bdbababd24354da51834c
Zoro <111187463+zororishi@users.noreply.github.com>
2023-12-02 18:59:28 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,13 @@ import { ThemeProvider } from "next-themes";
 import Navbar from "../components/navbar";
 import "../css/tailwind.css";
 import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Head>
+    <meta name="google-adsense-account" content="ca-pub-7427894411448013">
+    </Head>
     <ThemeProvider attribute="class">
     <Script async="true"
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7427894411448013"
