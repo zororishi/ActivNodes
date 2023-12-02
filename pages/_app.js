3342a03c6ab8c59ea9211a1d895fe5504cd795c0
Zoro <111187463+zororishi@users.noreply.github.com>
2023-12-02 18:36:04 +0530

    Update _app.js
    maybe added ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "next-themes";
 import Navbar from "../components/navbar";
 import "../css/tailwind.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      
+    <Script async="true"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7427894411448013"
+            crossorigin="anonymous" />
       <Component {...pageProps} />
     </ThemeProvider>
   );
