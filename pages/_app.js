aa8550a7ed29dce5b16d914a9e8b53215cd7dffc
Zoro <111187463+zororishi@users.noreply.github.com>
2023-12-02 19:09:20 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,6 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Head>
-    <meta name="google-adsense-account" content="ca-pub-7427894411448013"/>
-    </Head>
     <ThemeProvider attribute="class">
     <Script async="true"
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7427894411448013"
